90a9fdaa0b865d770d2ff9f307f85d84791bead0
Nethra S <nethrasshettar@gmail.com>
2024-02-08 11:39:43 +0530

    Code for the Problem Statement

--- Calculator2.java
+++ Calculator2.java
@@ -11,6 +11,14 @@ class Calculator1{
     float division(float operand_1,float operand_2){
         return(operand_1 / operand_2);
     }
+    float square(float operand_1){
+        return(operand_1*operand_1);
+    }
+    float cube(float operand_1){
+        return(operand_1*operand_1*operand_1);
+    }
+
+
     public static void main(String[] args){
         float operand_1 = 10;
         float operand_2 = 5;
@@ -23,6 +31,12 @@ class Calculator1{
         System.out.println("Result of multiplication is:" +mul_result);
         float div_result = calc.division(operand_1,operand_2);
         System.out.println("Result of division is:" +div_result);
+
+
+        float square_result = calc.square(operand_1);
+        System.out.println("Result of square is:" +square_result);
+        float cube_result = calc.cube(operand_1);
+        System.out.println("Result of cube is:" +cube_result);
 
 
     }
